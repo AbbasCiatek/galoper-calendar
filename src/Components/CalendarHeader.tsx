05a053991c595409e6c4a3467bf22b13f3205986
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-12 16:36:32 +0300

    displaying range date according to View

--- src/Components/CalendarHeader.tsx
+++ src/Components/CalendarHeader.tsx
@@ -2,6 +2,7 @@
 import NavigationArrows from "@/Components/HeaderComponents/NavigationArrows.tsx";
 import MonthYear from "@/Components/HeaderComponents/MonthYear.tsx";
 
+
 export default function CalendarHeader() {
     return(
     <div className='flex flex-col border p-4 m-4 rounded-t-lg '>
--- src/Components/HeaderComponents/NavigationArrows.tsx
+++ src/Components/HeaderComponents/NavigationArrows.tsx
@@ -1,10 +1,35 @@
 import {ChevronRightIcon, ChevronLeftIcon, } from "lucide-react"
 import { Button } from "@/components/ui/button"
+import {useContext} from "react";
+import ViewContext from "@/context.ts";
+import type {Views} from "@/types.ts";
+import {formatDate} from "date-fns";
+
 
 export default function NavigationArrows() {
-
+    const today =new Date();
+    const view = useContext(ViewContext);
     const handleLeftClick = () => {alert("left arrow click")}
     const handleRightClick = () => {alert("right arrow click")}
+
+    //display week start->end
+    const rangeDisplayer = (view:Views)=>{
+        switch(view){
+            case "day":
+                return (<span className=" px-1 font-extralight">{formatDate(today,'MMM, dd yyyy ')}</span>)
+            case "agenda":
+                return (<span className=" px-1 font-extralight">{formatDate(today,"MMM, yyyy")}</span>)
+            case "week":
+                return (<span className=" px-1 font-extralight">{formatDate(today,"")}</span>)
+            case "month":
+                return (<span className=" px-1 font-extralight">{formatDate(today,"MMM, yyyy")}</span>)
+            case "year":
+                return (<span className=" px-1 font-extralight">{formatDate(today,"yyyy")}</span>)
+            default:
+                return (<span className=" px-1 font-extralight">{formatDate(today,"MMM, yyyy")}</span>)
+
+        }
+    };
 
 
     return (
@@ -12,10 +37,7 @@ export default function NavigationArrows() {
             <Button variant="secondary" size="icon" className="size-8 cursor-pointer " onClick={handleLeftClick} >
                 <ChevronLeftIcon/>
             </Button>
-
-            <span className=" px-1 font-extralight">from-date</span>
-            -
-            <span className=" px-1 font-extralight">to-date</span>
+            {rangeDisplayer(view)}
             <Button variant="secondary" size="icon" className="size-8 cursor-pointer " onClick={handleRightClick} >
             <ChevronRightIcon />
             </Button>
