703fd8df04e59118a95cd0bcd236c4aa366238ef
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-12 16:11:08 +0300

    rebased on main and formatted
    resolving Conflicts
    
    change styles
    
    Year View Container
    
    Revert "add .editorconfig for consistent code formatting"
    
    This reverts commit 345db125eb860d24afb7a589debf4e4f1241e4c0.
    
    removed today constant, mono used types and Prop types from types.ts, and global font for header bar. Also used kebab naming for files
    
    reformatted file
    
    deleted unnecessary file
    
    Header Bar essentials
    
    types and context for views creation
    
    Header Component Creation
    
    removed today constant, mono used types and Prop types from types.ts, and global font for header bar. Also used kebab naming for files
    
    deleted unnecessary file
    
    Header Bar essentials
    
    types and context for views creation

--- src/components/calendar-header.tsx
+++ src/components/calendar-header.tsx
@@ -1,5 +1,5 @@
 export default function CalendarHeader() {
   return (
-    <div className="flex items-center border p-5 m-5 rounded-t-2xl"/>
+    <div className="flex items-center border p-5 m-5 rounded-t-2xl font-bold"/>
   )
 }
