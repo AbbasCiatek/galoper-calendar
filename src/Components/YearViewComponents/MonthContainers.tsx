46cadef4bd4386b00c44268f2a7381f0a512a353
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-15 16:33:18 +0300

    month displayer without onClick handler

--- src/Components/YearViewComponents/MonthContainers.tsx
+++ src/Components/YearViewComponents/MonthContainers.tsx
@@ -1,4 +1,4 @@
-
+import MonthNameDisplayer from "./MonthNameDisplayer"
 import getArrayMonth from "@/Functions/getArrayMonth.ts";
 
 
@@ -9,8 +9,8 @@ export default function MonthContainers() {
     const months = getArrayMonth(date);
     console.log(months);
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 ">
-
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-12 py-4 ">
+            {months.map((month, i) => (<MonthNameDisplayer key={i} month={month} />))}
         </div>
     );
 };
