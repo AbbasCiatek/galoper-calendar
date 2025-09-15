86ac0dabc55546b91f9109597b3630d36de84d5c
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-15 10:51:26 +0300

    Arrows Function are handled Correctly

--- src/Components/HeaderComponents/MonthYear.tsx
+++ src/Components/HeaderComponents/MonthYear.tsx
@@ -1,9 +1,8 @@
 import {formatDate} from "date-fns";
-import {useContext} from "react";
-import {DateContext} from "@/context.ts";
+import {useCalendar} from "@/context/context.tsx";
 
 export default function MonthYear(){
-const date:Date = useContext(DateContext);
+const {date} = useCalendar();
 
     return (
         <p className="font-extrabold text-2xl pb-1 " >{formatDate(date,'MMMM yyyy')}</p>
