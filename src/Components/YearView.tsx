f1bd47e3d0723b9c1133eb52be757f59c4ddd4cb
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-15 15:28:49 +0300

    change styles

--- src/Components/YearView.tsx
+++ src/Components/YearView.tsx
@@ -1,7 +1,7 @@
 
 export default function YearView() {
     return (
-        <div className=" m-4 p-4 rounded-b-2xl border border-gray-300 flex flex-col items-center justify-center ">
+        <div className=" m-4 p-4 rounded-b-2xl border border-gray-300 flex  justify-center ">
         </div>
     );
 }
