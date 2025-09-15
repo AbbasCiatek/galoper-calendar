8d87482fc1f242265a0cac3268eab216019355d9
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-15 16:08:30 +0300

    array of months

--- src/Components/YearView.tsx
+++ src/Components/YearView.tsx
@@ -1,7 +1,9 @@
+import MonthContainers from "@/Components/YearViewComponents/MonthContainers.tsx";
 
 export default function YearView() {
     return (
         <div className=" m-4 p-4 rounded-b-2xl border border-gray-300 flex flex-col items-center justify-center ">
+            <MonthContainers/>
         </div>
     );
 }
