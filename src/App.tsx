6d09bf13ddc0411eee99b5058daff24a2a0f36fa
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-16 10:44:48 +0300

    month-view-initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import CalendarHeader from "@/Components/calendar-header.tsx";
 import {CalendarContextProvider} from "@/context/calendar-context.tsx";
+import MonthView from "@/Components/MonthView.tsx";
 
 function App() {
-    return (
-        <CalendarContextProvider  >
-            <CalendarHeader />
-        </CalendarContextProvider>
-    )
+  return (
+    <CalendarContextProvider>
+      <CalendarHeader/>
+      <MonthView/>
+    </CalendarContextProvider>
+  )
 }
 
 export default App
