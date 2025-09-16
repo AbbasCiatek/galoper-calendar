4e44971269cf9aeb4c91ff95134e019bc0530f03
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-16 14:52:57 +0300

    days displayed in month view

--- src/dateHelpers.ts
+++ src/dateHelpers.ts
@@ -16,30 +16,41 @@ export default function daysInMonth(date: Date) {
 		start: firstDayOfMonth,
 		end: lastDayOfMonth,
 	});
+import {addMonths, eachDayOfInterval, endOfMonth, getDay, startOfMonth, subMonths} from "date-fns";
 
-	return { daysInMonth, indexOfFirstDay };
+export default function daysInMonth(date: Date){
+    const firstDayOfMonth = startOfMonth(date);
+    const lastDayOfMonth = endOfMonth(date);
+    //the Start of the week is Monday
+    const indexOfDays =((getDay(firstDayOfMonth)+6)%7);
+    const daysInMonth = eachDayOfInterval({
+        start:firstDayOfMonth,
+        end:lastDayOfMonth,
+    })
+
+    return {daysInMonth,indexOfDays};
 }
 
-export function numberOfDisplayedDaysOfNextMonth(
-	daysOfCurrentMonth: Date[],
-	indexOfCurrentMonthInWeekdays: number,
-) {
-	const lengthOfCurrentMonth = daysOfCurrentMonth.length;
-	if (lengthOfCurrentMonth + indexOfCurrentMonthInWeekdays > 35)
-		return 42 - lengthOfCurrentMonth - indexOfCurrentMonthInWeekdays;
-	return 35 - lengthOfCurrentMonth - indexOfCurrentMonthInWeekdays;
+export function  numberOfDisplayedDaysOfNextMonth(daysOfCurrentMonth:Date[],indexOfCurrentMonthInWeekdays:number) {
+    const lengthOfCurrentMonth = daysOfCurrentMonth.length;
+    if( lengthOfCurrentMonth+ indexOfCurrentMonthInWeekdays > 35 )
+        return  42 - lengthOfCurrentMonth-indexOfCurrentMonthInWeekdays;
+        return 35 - lengthOfCurrentMonth - indexOfCurrentMonthInWeekdays;
 }
 
-export function arrayOfDaysOfNextMonth(date: Date) {
-	const nextMonth = addMonths(date, 1);
-	console.log(nextMonth);
-	return daysInMonth(nextMonth);
+
+export function arrayOfDaysOfNextMonth(date:Date) {
+    const nextMonth =addMonths(date,1);
+    console.log(nextMonth);
+    return  daysInMonth(nextMonth);
 }
 export function arrayOfDaysOfPrevMonth(date: Date) {
 	const prevMonth = subMonths(date, 1);
 	return daysInMonth(prevMonth);
 }
 
+export function numberOfDisplayedDaysOfPrevMonth(currentDate:Date,indexOfCurrentMonthInWeekdays:number) {
+    return arrayOfDaysOfPrevMonth(currentDate).daysInMonth.length - indexOfCurrentMonthInWeekdays;
 export function numberOfDisplayedDaysOfPrevMonth(
 	currentDate: Date,
 	indexOfCurrentMonthInWeekdays: number,
