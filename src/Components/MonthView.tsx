be6e0e704dc7ee66aae5dfccde198921d893e368
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-16 11:03:16 +0300

    div updated

--- src/Components/MonthView.tsx
+++ src/Components/MonthView.tsx
@@ -1,6 +1,8 @@
 export default function MonthView() {
     return (
-        <div className="flex shadow-lg">
+        <div className="container m-auto shadow-lg">
+            <div className="grid grid-cols-7 text-xl">
+            </div>
         </div>
     );
 }
