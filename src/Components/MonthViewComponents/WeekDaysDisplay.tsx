74d196700024ed6f31e86ed10bc4909d226df865
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-16 11:02:09 +0300

    Weekdays display in monthview

---
+++ src/Components/MonthViewComponents/WeekDaysDisplay.tsx
@@ -0,0 +1,11 @@
+import {WeekDays} from "@/helpers.tsx";
+export default function WeekDaysDisplay() {
+    return (<>
+        {WeekDays.map((day) => {
+                return (
+                    <div className="text-center" key={day}>{day.slice(0,3)} </div>
+                )
+            })}
+        </>
+    );
+}
