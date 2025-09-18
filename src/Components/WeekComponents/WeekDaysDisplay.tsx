dd75b2230a9b314673976551d506cc94337e4151
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-18 15:17:11 +0300

    change styles in weekDaysDisplay

--- src/Components/WeekComponents/WeekDaysDisplay.tsx
+++ src/Components/WeekComponents/WeekDaysDisplay.tsx
@@ -2,23 +2,35 @@ import {formatDate, isToday} from "date-fns";
 import {daysOfWeek} from "@/dateHelpers.ts";
 import {clsx} from "clsx";
 
-export default function WeekDaysDisplay({date}:{date:Date}) {
-
+export default function WeekDaysDisplay({date}: { date: Date }) {
     const weekDays = daysOfWeek(date);
+
+    function handleClickedDate(day: Date) {
+        //must navigate to clicked date in day views
+        console.log(day);
+    }
+
     return (
         <div className="grid flex-1 grid-cols-7 divide-x border-l   ">
             {weekDays.map((day, index) => (
-                <div className="py-2 flex justify-center">
-                    <div
-                        key={index}
-                        className={clsx(
-                            "flex flex-col justify-center items-center text-xs font-medium size-12 rounded-full",
-                            isToday(day) ? "bg-blue-400 text-white" : "text-muted-foreground"
-                        )}>
-                        <span className="leading-none">{formatDate(day, "EE")}</span>
-                        <span className={clsx("font-semibold  leading-none", isToday(day) ? 'text-white':'text-foreground')}>
-                                {formatDate(day, "d")}
-                               </span>
+                <div key={index} className="pt-2 pb-1 px-2">
+                    <div className="flex flex-col items-center text-xs font-medium  ">
+                        <div className="flex flex-col items-center gap-1">
+            <span className={clsx("leading-none ",
+                isToday(day) ? "text-blue-500" : "text-muted-foreground")}>
+                {formatDate(day, "EE").toUpperCase()}
+            </span>
+                            <button
+                                onClick={() => handleClickedDate(day)}
+                                className={clsx("flex text-gray-700  justify-center cursor-pointer items-center size-10 rounded-full",
+                                    isToday(day) ? ' bg-blue-500 hover:bg-blue-600  text-white'
+                                        : ' hover:bg-gray-200 hover:text-black text-foreground')}
+                            >
+                                  <span className=" leading-none font-bold text-lg">
+                                      {formatDate(day, "d")}
+                                  </span>
+                            </button>
+                        </div>
                     </div>
                 </div>
 
