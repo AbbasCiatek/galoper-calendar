d8ce3887d2f2cfba48abdd0f9ada032cab0db88b
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-18 12:49:53 +0300

    destructured week days display and created datedisplaylayout

--- src/Components/WeekView.tsx
+++ src/Components/WeekView.tsx
@@ -1,12 +1,15 @@
 import WeekDaysDisplay from "@/Components/WeekComponents/WeekDaysDisplay.tsx";
 import WeekViewContainer from "@/Components/WeekComponents/WeekViewContainer.tsx";
+import DateDisplayLayout from "@/Components/WeeKDayViewCommonComponents/DateDisplayLayout.tsx";
 export default function WeekView({date}:{date:Date}) {
 
     return (
         <>
             <div className="flex flex-col ">
                 <div>
+                    <DateDisplayLayout>
                     <WeekDaysDisplay date={date}/>
+                    </DateDisplayLayout>
                     {/*AllDayEventsDisplay*/}
                 </div>
                 <WeekViewContainer date={date} />
