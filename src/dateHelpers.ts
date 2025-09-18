2be58f23e4d9fa77c738a34857382d604b6ddf5b
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-18 11:44:47 +0300

    Day and week commons

---
+++ src/dateHelpers.ts
@@ -0,0 +1,9 @@
+import {eachDayOfInterval, endOfWeek, startOfWeek} from "date-fns";
+
+export function daysOfWeek(date:Date) {
+         return eachDayOfInterval(
+        {
+            start:startOfWeek(date,{weekStartsOn:1}),
+            end:endOfWeek(date,{weekStartsOn:1}),
+        });
+}
