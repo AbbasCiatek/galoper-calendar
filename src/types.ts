f014d686745e1e77081b046449ba79c0f06a3230
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-19 14:34:28 +0300

    zod schema, zustand store and event type

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,19 @@
 export type ViewTypes = "day" | "week" | "month" | "year" | "agenda";
+export type COLORS = "blue" | "green" | "red" | "yellow" | "purple" | "orange" | "gray";
+
+export type Event = {
+    id: string;
+    title: string;
+    description: string;
+    startDate: Date;
+    endDate: Date;
+    color: COLORS;
+    isAllDay? : boolean;
+};
+export type Events = Event[];
+export type EventsStore = {
+    events: Events;
+    addEvent: (event: Event) => void;
+    editEvent: (id: string, edited: Partial<Event>) => void;
+    removeEvent: (id: string) => void;
+};
