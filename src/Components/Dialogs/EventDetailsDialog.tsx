dc2fac32595f722229aadf41d707ee6050a81152
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-22 09:44:34 +0300

    took the color constants into helper folder

--- src/Components/Dialogs/EventDetailsDialog.tsx
+++ src/Components/Dialogs/EventDetailsDialog.tsx
@@ -8,34 +8,13 @@ import {Button} from "@/components/ui/button.tsx";
 import AddEditEventDialog from "@/Components/Dialogs/Add-Edit-EventDialog.tsx";
 import useEventStore from "@/EventStore.ts";
 import {toast} from "sonner";
+import {detailsFromColor, titleFromColor} from "@/helpers.ts";
+
 type EventDetailsDialogProps = {
     children: ReactNode;
     event: Event;
 }
-const titleFromColor = (colorSelected:string)=>{
-    switch(colorSelected){
-        case "red":return "text-red-500";
-        case "green":return"text-green-500";
-        case "yellow":return"text-yellow-500";
-        case "blue":return"text-blue-500";
-        case "purple":return"text-purple-500";
-        case "orange":return"text-orange-500";
-        case "gray":return"text-gray-500";
-        default:return "text-gray-800"
-    }
-}
-const detailsFromColor = (colorSelected:string)=>{
-    switch(colorSelected){
-        case "red":return "text-red-300";
-        case "green":return"text-green-300";
-        case "yellow":return"text-yellow-300";
-        case "blue":return"text-blue-300";
-        case "purple":return"text-purple-300";
-        case "orange":return"text-orange-300";
-        case "gray":return"text-gray-300";
-        default:return "text-white"
-    }
-}
+
 export default function EventDetailsDialog({children,event}: EventDetailsDialogProps) {
     const [isOpen, setIsOpen] = useState(false);
     const onClose = () =>setIsOpen(false);
