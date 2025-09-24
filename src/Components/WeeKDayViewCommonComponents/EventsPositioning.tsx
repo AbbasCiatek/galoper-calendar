73375a9970561db31165fd74a98533f46d616147
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-24 23:11:54 +0300

    Event displayed on week view

---
+++ src/Components/WeeKDayViewCommonComponents/EventsPositioning.tsx
@@ -0,0 +1,36 @@
+import type {Events} from "@/types.ts";
+import {positionEventsWeekDayView} from "@/dateHelpers.ts";
+import EventWeekBlock from "@/Components/WeekComponents/EventWeekBlock.tsx";
+import {backgroundFromColor} from "@/helpers.ts";
+
+type Props= {
+    singleDayEvents: Events,
+    day:Date,
+}
+
+export default function EventsPositioning({singleDayEvents,day}: Props) {
+
+    const positioning = positionEventsWeekDayView(singleDayEvents, day);
+
+    return (
+        <>
+            {positioning.map(p=>{
+
+                const bgColor=backgroundFromColor(p.event.color);
+                return(
+                    <div
+                        key={p.event.id}
+                        className={` rounded-lg ${bgColor} text-xs text-white  absolute `}
+                        style={{
+                            top: `${p.top}%`,
+                            height: `${p.height}%`,
+                            left: `${p.left}%`,
+                            width: `${p.width}%`,
+                        }}
+                    >
+                        <EventWeekBlock event={p.event}  />
+                    </div>
+                )})}
+        </>
+    );
+}
---
+++ src/Components/WeekComponents/EventWeekBlock.tsx
@@ -0,0 +1,15 @@
+import type {Event} from "@/types.ts";
+import {formatDate, isSameDay} from "date-fns";
+
+export default function EventWeekBlock({event}: { event:Event }) {
+
+    return (
+        <div >
+            <div className="px-4">{event.title}</div>
+            <div className="px-4">  {isSameDay(event.startDate, event.endDate)
+                ? `${formatDate(event.startDate, "hh:mm a")} - ${formatDate(event.endDate, "hh:mm a")}`
+                : `${formatDate(event.startDate, "do, hh:mm a")} - ${formatDate(event.endDate, "hh:mm a")}`}
+            </div>
+        </div>
+    )
+}
