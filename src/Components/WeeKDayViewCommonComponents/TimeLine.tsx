a53760b870bd43c461b2ef58bfada45b00f70976
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-25 11:03:34 +0300

    designing changes

--- src/Components/WeeKDayViewCommonComponents/TimeLine.tsx
+++ src/Components/WeeKDayViewCommonComponents/TimeLine.tsx
@@ -11,17 +11,12 @@ export default function TimeLine() {
         const minute = currentTime.getHours() * 60 + currentTime.getMinutes();
         return (minute / minutesInDay) * 100;
     }
-    // const formatTime = ()=>{
-    //     return formatDate(currentTime,'h:mm aaa');
-    // }
     return (
         <div className="group">
-            <div className="pointer-events-none absolute inset-x-0 z-50 border-t border-red-500 "
+            <div className="pointer-events-none absolute inset-x-0 z-50 border-t border-black "
                  style={{ top: `${getCurrentPositionTime()}%` }}>
-                <div className=" absolute -left-2 -top-1.5 size-3  rounded-full bg-red-500 "></div>
-                    {/*<div className=" opacity-35 absolute left-[-72px] flex w-16 -translate-y-1/2 justify-end bg-background pr-1 text-xs font-medium text-primary">*/}
-                    {/*    {formatTime()}*/}
-                    {/*</div>*/}
+                <div className=" absolute -left-2 -top-1.5 size-3  rounded-full bg-black "></div>
+
             </div>
         </div>
     );
