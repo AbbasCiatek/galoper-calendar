a7d416bf75ab8a968f03356870848a14b679f167
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-25 11:29:14 +0300

    designing

--- src/Components/WeeKDayViewCommonComponents/HoursColumn.tsx
+++ src/Components/WeeKDayViewCommonComponents/HoursColumn.tsx
@@ -5,9 +5,9 @@ export default function HoursColumn() {
     return (
         <div className="relative w-18">
             {Hours.map((hour, index) => (
-                <div key={hour} className="relative h-12" >
-                    <div className="absolute -top-3 right-2 flex h-6 items-center">
-                        {index !== 0 && <span className="text-xs"> {formatDate(new Date().setHours(hour), "hh a")}</span>}
+                <div key={hour} className="relative h-24" >
+                    <div className="absolute -top-6 right-2 flex h-12 items-center">
+                        {index !== 0 && <span className="text-xs text-muted-foreground"> {formatDate(new Date().setHours(hour), "HH")}</span>}
                     </div>
                 </div>
             ))}
