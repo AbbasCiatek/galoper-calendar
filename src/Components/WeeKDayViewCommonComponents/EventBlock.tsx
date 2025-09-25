ace7254885ba1ce5f19a75d210469e9db41be882
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-25 11:29:14 +0300

    designing

---
+++ src/Components/WeeKDayViewCommonComponents/EventBlock.tsx
@@ -0,0 +1,16 @@
+import type {Event} from "@/types.ts";
+import {formatDate} from "date-fns";
+
+export default function EventBlock({event}: { event:Event }) {
+
+    return (
+        <div >
+            <div className="truncate font-semibold">
+                {event.title}
+            </div>
+            <div className="px-1">
+                {`${formatDate(event.startDate, " hh:mm a")} - ${formatDate(event.endDate, "hh:mm a")}`}
+            </div>
+        </div>
+    )
+}
