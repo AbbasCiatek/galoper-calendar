efccf7e1b75724caf987c4ff8ecfb08636e3b834
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-26 15:03:58 +0300

    event color

--- src/Components/Events/EventBullet.tsx
+++ src/Components/Events/EventBullet.tsx
@@ -7,19 +7,19 @@ export function EventBullet({color}: { color:COLORS }) {
     const bulletStyle = (colorSelected:COLORS)=>{
         switch (colorSelected) {
             case "red":
-                return "size-2 rounded-full bg-red-600 dark:bg-red-500";
+                return "w-2 h-2 rounded-full bg-red-600 dark:bg-red-500";
             case "green":
-                return "size-2 rounded-full bg-green-600 dark:bg-green-500";
+                return "w-2 h-2 rounded-full bg-green-600 dark:bg-green-500";
             case "yellow":
-                return "size-2 rounded-full bg-yellow-600 dark:bg-yellow-500";
+                return "w-2 h-2 rounded-full bg-yellow-600 dark:bg-yellow-500";
             case "blue":
-                return "size-2 rounded-full bg-blue-600 dark:bg-blue-500";
+                return "w-2 h-2 rounded-full bg-blue-600 dark:bg-blue-500";
             case "purple":
-                return  "size-2 rounded-full bg-purple-600 dark:bg-purple-500";
+                return  "w-2 h-2 rounded-full bg-purple-600 dark:bg-purple-500";
             case "orange":
-                return "size-2 rounded-full bg-orange-600 dark:bg-orange-500";
+                return "w-2 h-2 rounded-full bg-orange-600 dark:bg-orange-500";
             default:
-                return "size-2 rounded-full bg-blue-600 dark:bg-blue-500";
+                return "w-2 h-2 rounded-full bg-blue-600 dark:bg-blue-500";
         }
     }
     return (
