0f588ec6b5ef8f97860c6dcf643129e289cf85be
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-29 15:05:08 +0300

    validation change

--- src/schema.ts
+++ src/schema.ts
@@ -3,7 +3,7 @@ import {isAfter} from "date-fns";
 
 export const eventSchema = z.object({
     title: z.string().min(1, "Title is required"),
-    description: z.string().min(1, "Description is required"),
+    description: z.string(),
     startDate: z.date({message: "Start date is required"}),
     endDate: z.date({message: "End date is required"}),
     color: z.enum(["blue", "green", "red", "yellow", "purple", "orange", "gray"], {message: "Color is required"}),
