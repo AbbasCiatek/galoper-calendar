dd20c931a2dd5fe646f4c6c159da85e08f2767ef
HusseinMShamas <https://github.com/HusseinMShamas>
2025-09-29 15:03:02 +0300

    validation change

--- src/schema.ts
+++ src/schema.ts
@@ -1,19 +1,21 @@
-import { z } from "zod";
+import { z} from "zod";
+import {isAfter} from "date-fns";
 
 export const eventSchema = z.object({
-  title: z.string().min(1, "Title is required"),
-  description: z.string().min(1, "Description is required"),
-  startDate: z.date({ message: "Start date is required" }),
-  endDate: z.date({ message: "End date is required" }),
-  color: z.enum(
-    ["blue", "green", "red", "yellow", "purple", "orange", "gray"],
-    { message: "Color is required" },
-  ).refine((data)=> isAfter(data.endDate,data.startDate) ,{
-      message: "The ending date must be after the starting date ",
-      path:["endDate"],
-  }).refine((data)=> (data.endDate.getTime() - data.startDate.getTime())> (60 *1000) ,{
-      message: "Minimum Event Duration is One Minute ",
-      path:["endDate"],
+    title: z.string().min(1, "Title is required"),
+    description: z.string().min(1, "Description is required"),
+    startDate: z.date({message: "Start date is required"}),
+    endDate: z.date({message: "End date is required"}),
+    color: z.enum(["blue", "green", "red", "yellow", "purple", "orange", "gray"], {message: "Color is required"}),
+}).refine((data)=> isAfter(data.endDate,data.startDate) ,{
+    message: "The ending date must be after the starting date ",
+    path:["endDate"],
+}).refine((data)=> (data.endDate.getTime() - data.startDate.getTime())> (60 *1000) ,{
+  message: "Minimum Event Duration is One Minute ",
+  path:["endDate"],
 });
+// type InputType = z.input<typeof eventSchema>;
+//type OutputType =z.output<typeof eventSchema>;
 
 export type EventFormData = z.infer<typeof eventSchema>
+    //InputType | OutputType;
