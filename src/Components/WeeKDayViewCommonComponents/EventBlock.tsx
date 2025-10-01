dc7b385dcdb7302d858a22880922c9c6ad2bf02e
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-01 14:19:18 +0300

    changed heigh and top of event from percentage to pixel unit

--- src/Components/WeeKDayViewCommonComponents/EventBlock.tsx
+++ src/Components/WeeKDayViewCommonComponents/EventBlock.tsx
@@ -1,20 +1,19 @@
 import type {Event} from "@/types.ts";
 import {formatDate} from "date-fns";
+// import ResizableEvent from "@/Components/ResizableEvent.tsx";
 
-export default function EventBlock({event}: { event:Event }) {
+export default function EventBlock({event,height}: { event:Event ,height:number }) {
 
     return (
-        <DraggableEvent event={event}>
-            <EventDetailsDialog event={event}>
-        <div >
+        // <ResizableEvent event={event} height={height} selectedDate={new Date()}>
+        <div className=" overflow-hidden text-ellipsis" >
             <div className="truncate font-semibold">
                 {event.title}
             </div>
-            <div className="px-1">
+            <div className="  px-1">
                 {`${formatDate(event.startDate, " hh:mm a")} - ${formatDate(event.endDate, "hh:mm a")}`}
             </div>
         </div>
-                </EventDetailsDialog>
-            </DraggableEvent>
+        // </ResizableEvent>
     )
 }
