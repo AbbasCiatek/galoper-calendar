d741327898f2731ac04658451aef7af0110b9538
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-02 12:01:42 +0300

    unifying colors

--- src/Components/WeekComponents/MultiDayEventDisplay.tsx
+++ src/Components/WeekComponents/MultiDayEventDisplay.tsx
@@ -1,5 +1,5 @@
-import { cn } from "@/lib/utils"; // className joiner
 import type {Event} from "@/types.ts";
+import {colorMap} from "@/helpers.ts";
 
 interface Props {
     event: Event;
@@ -9,16 +9,7 @@ interface Props {
 
 export function MultiDayEventDisplay({ event, position }: Props) {
 
-    const colorMap:Record<string, string>={
-        red: 'bg-red-200 text-red-500 border border-red-500 ',
-        green: 'bg-green-200 text-green-500 border border-green-500 ',
-        orange: 'bg-orange-200 text-orange-500 border border-orange-500 ',
-        yellow: 'bg-yellow-200 text-yellow-500 border border-yellow-500 ',
-        purple: 'bg-purple-200 text-purple-500 border border-purple-500 ',
-        gray: 'bg-gray-200 text-gray-500 border border-gray-500 ',
-        blue: 'bg-blue-200 text-blue-500 border border-blue-500 ',
-    }
-
+    const eventStyle = colorMap[event.color];
     const positionClasses = {
         none: "rounded-md",
         first: "rounded-l-md border-r-0  ",
@@ -29,7 +20,7 @@ export function MultiDayEventDisplay({ event, position }: Props) {
     return (
         // <DraggableEvent event={event}>
         //     <EventDetailsDialog event={event}>
-        <div className={  `  flex items-center h-6.5 px-2 text-xs font-medium truncate cursor-pointer ${positionClasses}  ${colorMap[event.color]} `}>
+        <div className={  `  flex items-center h-6.5 px-2 text-xs font-medium truncate cursor-pointer ${positionClasses}  ${eventStyle} `}>
             {position === "first" || position === "none" ? event.title : null}
         </div>
                 // </EventDetailsDialog>
