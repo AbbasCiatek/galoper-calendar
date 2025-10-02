2093b300ffe490ab20a15e25a5bf816402deb7a2
Abbas Srour <abbas.mj.srour@gmail.com>
2025-10-02 13:46:44 +0300

    reformatted file

--- src/Components/CalendarHeader.tsx
+++ src/Components/CalendarHeader.tsx
@@ -1,9 +1,5 @@
-
-
 export default function CalendarHeader() {
-
-    return (
-        <div className="flex items-center border p-5 m-5 rounded-t-2xl font-bold">
-        </div>
-    )
+  return (
+    <div className="flex items-center border p-5 m-5 rounded-t-2xl font-bold"/>
+  )
 }
