164f65c052aa3d1ace9654986d07078be726319d
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-03 13:34:20 +0300

    wrapping app with container

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,10 @@ import { CalendarContextProvider } from "@/context/calendar-context.tsx";
 function App() {
 	return (
 		<CalendarContextProvider>
+            <div className="container m-auto mt-5">
 			<CalendarHeader />
 			<MonthView />
+            </div>
 		</CalendarContextProvider>
 	);
 }
