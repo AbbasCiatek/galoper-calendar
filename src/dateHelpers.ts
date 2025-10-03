8b0c4b1de1370d4ca3b3668d16bb2c631c13bc3c
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-03 16:26:53 +0300

    resolving rebase Conflicts

--- src/dateHelpers.ts
+++ src/dateHelpers.ts
@@ -1,23 +1,30 @@
-import {addMonths, eachDayOfInterval, endOfMonth, getDay, startOfMonth, startOfYear} from "date-fns";
+import {
+	addMonths,
+	eachDayOfInterval,
+	endOfMonth,
+	getDay,
+	startOfMonth,
+	startOfYear,
+} from "date-fns";
 
-export  function daysInMonth(date: Date){
-    const firstDayOfMonth = startOfMonth(date);
-    const lastDayOfMonth = endOfMonth(date);
-    const firstDayIndex = getDay(firstDayOfMonth);
-    const daysInMonth = eachDayOfInterval({
-        start:firstDayOfMonth,
-        end:lastDayOfMonth,
-    })
+export function daysInMonth(date: Date) {
+	const firstDayOfMonth = startOfMonth(date);
+	const lastDayOfMonth = endOfMonth(date);
+	const firstDayIndex = getDay(firstDayOfMonth);
+	const daysInMonth = eachDayOfInterval({
+		start: firstDayOfMonth,
+		end: lastDayOfMonth,
+	});
 
-    return {daysInMonth,firstDayIndex};
+	return { daysInMonth, firstDayIndex };
 }
 
-export  function getArrayMonth(date: Date) {
-    const year = startOfYear(date);
-    let i = 0;
-    const months: Date[] = [];
-    for (i = 0; i < 12; i++){
-        months.push( addMonths(year, i));
-    }
-    return months;
+export function getArrayMonth(date: Date) {
+	const year = startOfYear(date);
+	let i = 0;
+	const months: Date[] = [];
+	for (i = 0; i < 12; i++) {
+		months.push(addMonths(year, i));
+	}
+	return months;
 }
--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,19 @@
 export type ViewTypes = "day" | "week" | "month" | "year" | "agenda";
-export type COLORS = "blue" | "green" | "red" | "yellow" | "purple" | "orange" | "gray";
+export type COLORS =
+	| "blue"
+	| "green"
+	| "red"
+	| "yellow"
+	| "purple"
+	| "orange"
+	| "gray";
 
 export type Event = {
-    id: string;
-    title: string;
-    description: string;
-    startDate: Date;
-    endDate: Date;
-    color: COLORS;
-    isAllDay? : boolean;
-};
-export type Events = Event[];
-export type EventsStore = {
-    events: Events;
-    getEventsByDateRange: (startDate:Date, endDate:Date) => Event[];
+	id: string;
+	title: string;
+	description: string;
+	startDate: Date;
+	endDate: Date;
+	color: COLORS;
+	isAllDay?: boolean;
 };
