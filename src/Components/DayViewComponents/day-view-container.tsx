782acb5f9311088a2a6e12ed935541fbc732798e
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-03 14:38:17 +0300

    resolving Conflicts + design changes + getting date from hook

---
+++ src/Components/DayViewComponents/day-view-container.tsx
@@ -0,0 +1,14 @@
+import DayTimeCell from "@/Components/DayViewComponents/day-time-cell.tsx";
+import HoursColumn from "@/Components/WeeKDayViewCommonComponents/hours-column.tsx";
+import { ScrollArea } from "@/components/ui/scroll-area.tsx";
+
+export default function DayViewContainer() {
+	return (
+		<ScrollArea className="h-[500px] md:h-[800px] border-b">
+			<div className="flex overflow-hidden">
+				<HoursColumn />
+				<DayTimeCell />
+			</div>
+		</ScrollArea>
+	);
+}
