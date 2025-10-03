f1e0fef82c039e0715e10fc421d6bcb72e7ae749
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-03 14:38:17 +0300

    resolving Conflicts + design changes + getting date from hook

---
+++ src/Components/WeeKDayViewCommonComponents/hours-column.tsx
@@ -0,0 +1,21 @@
+import { formatDate } from "date-fns";
+import { Hours } from "@/helpers.ts";
+
+export default function HoursColumn() {
+	return (
+		<div className="relative w-18">
+			{Hours.map((hour, index) => (
+				<div key={hour} className="relative h-24">
+					<div className="absolute -top-6 right-2 flex h-12 items-center">
+						{index !== 0 && (
+							<span className="text-xs text-muted-foreground">
+								{" "}
+								{formatDate(new Date().setHours(hour), "HH")}
+							</span>
+						)}
+					</div>
+				</div>
+			))}
+		</div>
+	);
+}
---
+++ src/Components/WeeKDayViewCommonComponents/time-line.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+export default function TimeLine() {
+	const [currentTime, setCurrentTime] = useState(new Date());
+	useEffect(() => {
+		const timer = setInterval(() => setCurrentTime(new Date()), 60 * 1000);
+		return () => clearInterval(timer);
+	}, []);
+	const getCurrentPositionTime = () => {
+		const minutesInDay = 24 * 60;
+		const minute = currentTime.getHours() * 60 + currentTime.getMinutes();
+		return (minute / minutesInDay) * 100;
+	};
+
+	return (
+		<div className="group">
+			<div
+				className="pointer-events-none absolute inset-x-0 z-50 border-t border-black "
+				style={{ top: `${getCurrentPositionTime()}%` }}
+			>
+				<div className=" absolute -left-2 -top-1.5 size-3  rounded-full bg-black "></div>
+			</div>
+		</div>
+	);
+}
