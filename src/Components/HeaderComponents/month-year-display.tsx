4fe329b73522435906593c755945fa5050770ad1
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-03 10:07:20 +0300

    kebab naming files and solving conflicts of rebase

---
+++ src/Components/HeaderComponents/month-year-display.tsx
@@ -0,0 +1,12 @@
+import { formatDate } from "date-fns";
+import { useCalendar } from "@/context/calendar-context.tsx";
+
+export default function MonthYearDisplay() {
+	const { date } = useCalendar();
+
+	return (
+		<p className="font-extrabold text-2xl pb-1 ">
+			{formatDate(date, "MMMM yyyy")}
+		</p>
+	);
+}
