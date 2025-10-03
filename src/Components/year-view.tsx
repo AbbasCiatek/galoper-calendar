e21335f748ea611fd8912055061002842b5ba769
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-03 13:59:01 +0300

    resolving Conflicts

--- src/Components/year-view.tsx
+++ src/Components/year-view.tsx
@@ -1,5 +1,9 @@
+import MonthContainers from "@/Components/YearViewComponents/month-containers.tsx";
+
 export default function YearView() {
 	return (
-		<div className=" m-4 p-4 rounded-b-2xl border border-gray-300 flex  justify-center "></div>
+		<div className=" m-4 p-4 rounded-b-2xl border border-gray-300 flex  justify-center ">
+			<MonthContainers />
+		</div>
 	);
 }
