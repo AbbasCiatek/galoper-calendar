66901ea0156d355cc31c58da05efbf3cf065ed05
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-03 10:16:36 +0300

    solve rebase conflicts

--- src/Components/HeaderComponents/month-year-display.tsx
+++ src/Components/HeaderComponents/month-year-display.tsx
@@ -1,12 +1,23 @@
 import { formatDate } from "date-fns";
+import { Badge } from "@/components/ui/badge.tsx";
 import { useCalendar } from "@/context/calendar-context.tsx";
 
 export default function MonthYearDisplay() {
-	const { date } = useCalendar();
+	const { date, view } = useCalendar();
+
+	//const eventCounter = getEventNumber(view,date,events);
+	const eventCounter = 10;
 
 	return (
-		<p className="font-extrabold text-2xl pb-1 ">
-			{formatDate(date, "MMMM yyyy")}
-		</p>
+		<div className="flex flex-row">
+			<p className="font-extrabold text-2xl pb-1">
+				{formatDate(date, "MMMM yyyy")}
+			</p>
+			<div className="mx-1">
+				<Badge variant="secondary" className="py-1 px-2">
+					{eventCounter} events
+				</Badge>
+			</div>
+		</div>
 	);
 }
