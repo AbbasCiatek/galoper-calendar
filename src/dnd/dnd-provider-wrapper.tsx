6869be370800e12795b157985d684c81fd2a5214
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-07 10:38:33 +0300

    kebab naming

--- src/dnd/dnd-provider-wrapper.tsx
+++ src/dnd/dnd-provider-wrapper.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import CustomDragLayer from "@/dnd/CustomDragLayer.tsx";
+import CustomDragLayer from "@/dnd/custom-drag-layer.tsx";
 
 export default function DndProviderWrapper({
 	children,
