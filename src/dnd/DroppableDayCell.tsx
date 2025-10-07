b6172ba2cbe6a0c54ed54d4243ed91c404a1b7f8
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-07 10:23:09 +0300

    formatting

--- src/dnd/DroppableDayCell.tsx
+++ src/dnd/DroppableDayCell.tsx
@@ -1,55 +1,57 @@
-"use client";
-
+import { differenceInMilliseconds } from "date-fns";
+import type { ReactNode, RefObject } from "react";
 import { useDrop } from "react-dnd";
-import {  differenceInMilliseconds } from "date-fns";
-
-
-import { cn } from "@/lib/utils";
 import { ItemTypes } from "@/dnd/DraggableEvents";
-
-import type { Event } from "@/types.ts";
-import type {ReactNode, RefObject} from "react";
 import useEventStore from "@/EventStore.ts";
+import type { Event } from "@/types.ts";
 
+type Props = {
+	date: Date;
+	children: ReactNode;
+};
 
-type Props ={
-    date: Date;
-    children:ReactNode;
-}
+export function DroppableDayCell({ date, children }: Props) {
+	const { editEvent } = useEventStore();
 
-export function DroppableDayCell({date, children }: Props) {
-    const {editEvent} = useEventStore();
+	const [drop] = useDrop(
+		() => ({
+			accept: ItemTypes.EVENT,
+			drop: (item: { event: Event }) => {
+				const droppedEvent = item.event;
 
-    const [{ isOver, canDrop }, drop] = useDrop(
-        () => ({
-            accept: ItemTypes.EVENT,
-            drop: (item: { event: Event }) => {
-                const droppedEvent = item.event;
+				const eventStartDate = new Date(droppedEvent.startDate);
+				const eventEndDate = new Date(droppedEvent.endDate);
 
-                const eventStartDate = new Date(droppedEvent.startDate);
-                const eventEndDate = new Date(droppedEvent.endDate);
+				const eventDurationMs = differenceInMilliseconds(
+					eventEndDate,
+					eventStartDate,
+				);
 
-                const eventDurationMs = differenceInMilliseconds(eventEndDate, eventStartDate);
+				const newStartDate = new Date(date);
+				newStartDate.setHours(
+					eventStartDate.getHours(),
+					eventStartDate.getMinutes(),
+					eventStartDate.getSeconds(),
+					eventStartDate.getMilliseconds(),
+				);
+				const newEndDate = new Date(newStartDate.getTime() + eventDurationMs);
 
-                const newStartDate = new Date(date);
-                newStartDate.setHours(eventStartDate.getHours(), eventStartDate.getMinutes(), eventStartDate.getSeconds(), eventStartDate.getMilliseconds());
-                const newEndDate = new Date(newStartDate.getTime() + eventDurationMs);
+				editEvent(droppedEvent.id, {
+					startDate: newStartDate,
+					endDate: newEndDate,
+				});
 
-                editEvent(droppedEvent.id,{startDate:newStartDate,endDate:newEndDate});
+				return { moved: true };
+			},
+			collect: (monitor) => ({
+				isOver: monitor.isOver(),
+				canDrop: monitor.canDrop(),
+			}),
+		}),
+		[date, editEvent],
+	);
 
-                return { moved: true };
-            },
-            collect: monitor => ({
-                isOver: monitor.isOver(),
-                canDrop: monitor.canDrop(),
-            }),
-        }),
-        [date, editEvent]
-    );
-
-    return (
-        <div ref={drop as unknown as RefObject<HTMLDivElement>}>
-            {children}
-        </div>
-    );
+	return (
+		<div ref={drop as unknown as RefObject<HTMLDivElement>}>{children}</div>
+	);
 }
