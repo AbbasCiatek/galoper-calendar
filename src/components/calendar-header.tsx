afb22f76d826801b5f116cd7a9ff4ec3d354c98e
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-09 10:02:01 +0300

    merged today button to date and arrows ,formatted and apply positioning for all the elements

--- src/components/calendar-header.tsx
+++ src/components/calendar-header.tsx
@@ -1,14 +1,9 @@
-import TodayButton from "@/components/header-components/today-button.tsx";
-
 import DateAndNavigators from "@/components/header-components/date-and-navigators.tsx";
 
 export default function CalendarHeader() {
   return (
     <div className="flex items-center border p-5 m-5 rounded-t-2xl">
-      <div className="flex flex-col ">
-        <DateAndNavigators />
-        <TodayButton />
-      </div>
+      <DateAndNavigators />
     </div>
   );
 }
--- src/components/header-components/date-and-navigators.tsx
+++ src/components/header-components/date-and-navigators.tsx
@@ -14,6 +14,14 @@ import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 export default function DateAndNavigators() {
   const { view, date, setDate } = useCalendar();
 
+  const today = new Date();
+
+  const handleTodayClick = (date: Date) => {
+    if (date !== today) {
+      setDate(today);
+    }
+  };
+
   const handleLeftClick = (view: ViewTypes, date: Date) => {
     const subtractedDate = DateSubtracterFunction(view, date);
     setDate(subtractedDate);
@@ -27,38 +35,52 @@ export default function DateAndNavigators() {
   const eventCounter = getNumberOfEvents(date, view);
 
   return (
-    <div>
-      <div className="flex flex-row">
-        <p className="font-extrabold text-2xl pb-1">
-          {formatDate(date, "MMMM yyyy")}
+    <div className="flex flex-row">
+      <button
+        type="button"
+        onClick={() => handleTodayClick(date)}
+        className="border text-center size-16 mr-2 pt-1 rounded-lg cursor-pointer font-bold text-gray-900 dark:bg-gray-900 dark:text-white"
+      >
+        {formatDate(today, "MMM").toUpperCase()}
+        <p className="pt-1.5 w-full h-9 bg-gray-900 rounded-b-lg pb-[2px] text-white dark:text-gray-900 dark:bg-white ">
+          {today.getDate()}
         </p>
-        <div className="mx-1">
-          <Badge variant="secondary" className="py-1 px-3">
-            {eventCounter} events
-          </Badge>
+      </button>
+      <div className="flex flex-col">
+        <div className="flex flex-row">
+          <p className="font-extrabold text-2xl pb-1">
+            {formatDate(date, "MMMM yyyy")}
+          </p>
+          <div className="mx-1">
+            <Badge variant="secondary" className="py-1 px-3">
+              {eventCounter} events
+            </Badge>
+          </div>
+        </div>
+        <div>
+          <Button
+            variant="secondary"
+            size="icon"
+            className="size-7 cursor-pointer mr-1"
+            onClick={() => {
+              handleLeftClick(view, date);
+            }}
+          >
+            <ChevronLeftIcon />
+          </Button>
+          <span className="text-gray-500">{rangeDisplayer(view, date)}</span>
+          <Button
+            variant="secondary"
+            size="icon"
+            className="size-7 cursor-pointer ml-1"
+            onClick={() => {
+              handleRightClick(view, date);
+            }}
+          >
+            <ChevronRightIcon />
+          </Button>
         </div>
       </div>
-      <Button
-        variant="secondary"
-        size="icon"
-        className="size-7 cursor-pointer mr-1"
-        onClick={() => {
-          handleLeftClick(view, date);
-        }}
-      >
-        <ChevronLeftIcon />
-      </Button>
-      <span className="text-gray-500">{rangeDisplayer(view, date)}</span>
-      <Button
-        variant="secondary"
-        size="icon"
-        className="size-7 cursor-pointer ml-1"
-        onClick={() => {
-          handleRightClick(view, date);
-        }}
-      >
-        <ChevronRightIcon />
-      </Button>
     </div>
   );
 }
