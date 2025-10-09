d879a8dfb3e253ae0389af695cf85fa12c812957
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-09 08:44:44 +0300

    rebased on main, solved biome errors + formatted

--- src/components/calendar-header.tsx
+++ src/components/calendar-header.tsx
@@ -1,9 +1,9 @@
-import ViewChanger from "@/Components/HeaderComponents/view-changer.tsx";
+import ViewChanger from "@/components/header-components/view-changer.tsx";
 
 export default function CalendarHeader() {
-	return (
-		<div className="flex items-center border p-5 m-5 rounded-t-2xl">
-			<ViewChanger />
-		</div>
-	);
+  return (
+    <div className="flex items-center border p-5 m-5 rounded-t-2xl">
+      <ViewChanger />
+    </div>
+  );
 }
