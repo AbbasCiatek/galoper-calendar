c879ba42ef334ffe159b046de37e47b7ec1f0173
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-10 16:10:57 +0300

    downloaded framer motion and added animation for events bullet

--- src/components/events/event-bullet.tsx
+++ src/components/events/event-bullet.tsx
@@ -1,24 +1,37 @@
-import { clsx } from "clsx";
+import { transition } from "@/lib/animations.ts";
 import type { COLORS } from "@/types.ts";
+import { clsx } from "clsx";
+import { motion } from "motion/react";
 
 export function EventBullet({ color }: { color: COLORS }) {
-	const bulletStyle = (colorSelected: COLORS) => {
-		switch (colorSelected) {
-			case "red":
-				return "w-2 h-2 rounded-full bg-red-600 dark:bg-red-500";
-			case "green":
-				return "w-2 h-2 rounded-full bg-green-600 dark:bg-green-500";
-			case "yellow":
-				return "w-2 h-2 rounded-full bg-yellow-600 dark:bg-yellow-500";
-			case "blue":
-				return "w-2 h-2 rounded-full bg-blue-600 dark:bg-blue-500";
-			case "purple":
-				return "w-2 h-2 rounded-full bg-purple-600 dark:bg-purple-500";
-			case "orange":
-				return "w-2 h-2 rounded-full bg-orange-600 dark:bg-orange-500";
-			default:
-				return "w-2 h-2 rounded-full bg-blue-600 dark:bg-blue-500";
-		}
-	};
-	return <div className={clsx(bulletStyle(color), "mt-0.5")} />;
+  const bulletStyle = (colorSelected: COLORS) => {
+    switch (colorSelected) {
+      case "red":
+        return "w-2 h-2 rounded-full bg-red-600 dark:bg-red-500";
+      case "green":
+        return "w-2 h-2 rounded-full bg-green-600 dark:bg-green-500";
+      case "yellow":
+        return "w-2 h-2 rounded-full bg-yellow-600 dark:bg-yellow-500";
+      case "blue":
+        return "w-2 h-2 rounded-full bg-blue-600 dark:bg-blue-500";
+      case "purple":
+        return "w-2 h-2 rounded-full bg-purple-600 dark:bg-purple-500";
+      case "orange":
+        return "w-2 h-2 rounded-full bg-orange-600 dark:bg-orange-500";
+      default:
+        return "w-2 h-2 rounded-full bg-blue-600 dark:bg-blue-500";
+    }
+  };
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: -5 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        delay: 0.25,
+        duration: 0.5,
+        ...transition,
+      }}
+      className={clsx(bulletStyle(color), "mt-0.5")}
+    />
+  );
 }
