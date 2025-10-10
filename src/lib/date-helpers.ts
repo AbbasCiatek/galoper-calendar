4a13b1f83632503bea3d5d78c2b4da723e1c62d6
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-10 10:18:29 +0300

    removed  default export from getArrayMonth function

--- src/lib/date-helpers.ts
+++ src/lib/date-helpers.ts
@@ -1,6 +1,6 @@
 import { addMonths, startOfYear } from "date-fns";
 
-export default function getArrayMonth(date: Date) {
+export function getArrayMonth(date: Date) {
   const year = startOfYear(date);
   const months: Array<Date> = [];
   for (let i = 0; i < 12; i++) {
