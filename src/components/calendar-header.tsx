90e2246926d701f96e32c3aa6168a52ce454085d
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-10 09:58:09 +0300

    added dark mode border color and deleted margin for calendar header div

--- src/components/calendar-header.tsx
+++ src/components/calendar-header.tsx
@@ -1,5 +1,5 @@
 export default function CalendarHeader() {
   return (
-    <div className="flex items-center border p-5 m-5 rounded-t-2xl font-bold"/>
-  )
+    <div className="flex items-center border dark:border-gray-200 p-5 border-b-0 rounded-t-2xl" />
+  );
 }
