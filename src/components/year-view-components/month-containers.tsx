c1f67edceef78d8945986dfe0bd6b7f3db592c83
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-10 11:14:26 +0300

    rebased month name displayer on year view month-containers

--- src/components/year-view-components/month-containers.tsx
+++ src/components/year-view-components/month-containers.tsx
@@ -1,11 +1,16 @@
 import { useCalendar } from "@/context/calendar-context.tsx";
 import { getArrayMonth } from "@/lib/date-helpers.ts";
+import { formatDate } from "date-fns";
 
 export default function MonthContainers() {
   const { date } = useCalendar();
   const months = getArrayMonth(date);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 " />
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 ">
+      {months.map((month) => (
+        <MonthNameDisplayer key={formatDate(month, "MMM")} month={month} />
+      ))}
+    </div>
   );
 }
