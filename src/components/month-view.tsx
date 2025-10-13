dfd46f138823bbe4b834ac0330989d2116ced715
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-13 10:30:08 +0300

    div of Month view changed

--- src/components/month-view.tsx
+++ src/components/month-view.tsx
@@ -1,7 +1,3 @@
 export default function MonthView() {
-  return (
-    <div className="border rounded-b-2xl">
-      <div className="grid grid-cols-7 text-xl" />
-    </div>
-  );
+  return <div className="border rounded-b-2xl" />;
 }
