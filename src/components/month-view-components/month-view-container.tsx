bbb5abecd0f2c94a686a006443f1b9378b6df119
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-15 14:14:09 +0300

    added useMemo for cells of Month and changed name from objectDays to cells

--- src/components/month-view-components/month-view-container.tsx
+++ src/components/month-view-components/month-view-container.tsx
@@ -2,22 +2,28 @@ import { useCalendar } from "@/context/calendar-context.tsx";
 import { getCalendarCellsOfMonth } from "@/lib/date-helpers.ts";
 import { clsx } from "clsx";
 import { formatDate, isMonday, isToday } from "date-fns";
+import { useMemo } from "react";
 
 export default function MonthViewContainer() {
   const { date } = useCalendar();
+  //for re-rendering only on year or month change
+  const monthDate = new Date(date.getFullYear(), date.getMonth(), 1);
 
-  const daysObject = getCalendarCellsOfMonth(date, true);
+  const cells = useMemo(
+    () => getCalendarCellsOfMonth(monthDate, true),
+    [monthDate],
+  );
 
   return (
     <div className="grid grid-cols-7 w-full h-full">
-      {daysObject.map((objectDay) => {
+      {cells.map((cell) => {
         return (
           <div
-            key={formatDate(objectDay.day, "do dd MMMM yyyy HH:mm")}
+            key={formatDate(cell.day, "do dd MMMM yyyy HH:mm")}
             className={clsx(
               "h-32 gap-1 border-l border-t",
-              isMonday(objectDay.day) && "border-l-0",
-              objectDay.currentMonth
+              isMonday(cell.day) && "border-l-0",
+              cell.currentMonth
                 ? "text-gray-800 dark:text-gray-200"
                 : "text-gray-400 dark:text-gray-500",
             )}
@@ -25,14 +31,14 @@ export default function MonthViewContainer() {
             <div
               className={clsx(
                 "h-6 px-1 text-xs font-semibold lg:px-2 ",
-                objectDay.currentMonth &&
-                  isToday(objectDay.day) &&
+                cell.currentMonth &&
+                  isToday(cell.day) &&
                   "flex w-6 translate-x-1 items-center justify-center rounded-full " +
                     "bg-gray-900 text-gray-200 dark:bg-gray-200 dark:text-gray-900 px-0 font-bold ",
               )}
             >
               {" "}
-              {formatDate(objectDay.day, "d")}
+              {formatDate(cell.day, "d")}
             </div>
           </div>
         );
