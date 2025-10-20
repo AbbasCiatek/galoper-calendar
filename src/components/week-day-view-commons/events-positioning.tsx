981e6a3ee394bf4ac7821991397808fd193594fd
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-20 12:21:39 +0300

    resolved conflicts after rebase, deleted default exports

---
+++ src/components/week-day-view-commons/events-positioning.tsx
@@ -0,0 +1,39 @@
+import { EventBlock } from "@/components/week-day-view-commons/event-block.tsx";
+import type { Event } from "@/event-store.ts";
+import { colorMap } from "@/helpers.ts";
+import { positionEventsWeekDayView } from "@/lib/date-helpers.ts";
+import { clsx } from "clsx";
+
+type Props = {
+  singleDayEvents: Array<Event>;
+  date: Date;
+};
+
+export function EventsPositioning({ singleDayEvents, date }: Props) {
+  const positioning = positionEventsWeekDayView(singleDayEvents, date);
+
+  return (
+    <>
+      {positioning.map((p) => {
+        return (
+          <button
+            type="button"
+            key={p.event.id}
+            className={clsx(
+              "border-2 rounded-lg  text-xs  absolute overflow-hidden",
+              colorMap[p.event.color],
+            )}
+            style={{
+              top: `${p.top}px`,
+              height: `${p.height}px`,
+              left: `${p.left}%`,
+              width: `${p.width}%`,
+            }}
+          >
+            <EventBlock event={p.event} height={p.height} />
+          </button>
+        );
+      })}
+    </>
+  );
+}
