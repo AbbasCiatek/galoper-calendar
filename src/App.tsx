4a08a2adcacfa4a1f85c796abb94e8c7dbdd3508
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-20 10:49:14 +0300

    solving conflict after rebase, kebab naming and deleting default exports

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { CalendarHeader } from "@/components/calendar-header.tsx";
 import { CalendarContextProvider } from "@/context/calendar-context.tsx";
-import WeekView from "@/Components/WeekView.tsx";
+import { WeekView } from "./components/week-view";
 
 function App() {
-    return (
-        <CalendarContextProvider  >
-            <div className="container m-auto mt-5">
-            <CalendarHeader />
-                <WeekView date={date}/>
-            </div>
-        </CalendarContextProvider>
-    )
+  return (
+    <CalendarContextProvider>
+      <div className="container m-auto mt-5">
+        <CalendarHeader />
+        <WeekView />
+      </div>
+    </CalendarContextProvider>
+  );
 }
 
-export default App
+export default App;
