b562ba69d421dcea5f2fcff4bc074fd68327a638
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-20 10:21:59 +0300

    kebab naming and consistant formating

---
+++ src/components/week-day-view-commons/date-display-layout.tsx
@@ -0,0 +1,12 @@
+import type { ReactNode } from "react";
+
+export default function DateDisplayLayout({
+  children,
+}: { children: ReactNode }) {
+  return (
+    <div className="relative z-20 flex border-b">
+      <div className="w-18" />
+      {children}
+    </div>
+  );
+}
---
+++ src/components/week-day-view-commons/time-cell.tsx
@@ -0,0 +1,20 @@
+import { Hours } from "@/helpers.ts";
+export default function TimeCells() {
+  return (
+    <>
+      {Hours.map((hour, index) => {
+        return (
+          <div key={hour} className="relative h-24">
+            {index !== 0 && (
+              <div className="pointer-events-none absolute  inset-x-0 top-0 border-b" />
+            )}
+            <div className="absolute  inset-x-0 top-0 h-12 cursor-pointer hover:bg-accent" />
+
+            <div className="pointer-events-none absolute inset-x-0 top-1/2 border-b   border-dashed" />
+            <div className="absolute inset-x-0 top-12 h-12 cursor-pointer  hover:bg-accent" />
+          </div>
+        );
+      })}
+    </>
+  );
+}
