42cfc6cf80c5b5cfdac7f454f34547b22f540c92
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-20 11:40:47 +0300

    solved conflict of rebasing, removed default exporting

---
+++ src/components/week-day-view-commons/event-positioning.tsx
@@ -0,0 +1,37 @@
+import { EventBlock } from "@/components/week-day-view-commons/event-block.tsx";
+import type { Event } from "@/event-store.ts";
+import { colorMap } from "@/helpers.ts";
+import { positionEventsWeekDayView } from "@/lib/date-helpers";
+
+type Props = {
+  singleDayEvents: Array<Event>;
+  date: Date;
+};
+
+export function EventsPositioning({ singleDayEvents, date }: Props) {
+  const positioning = positionEventsWeekDayView(singleDayEvents, date);
+
+  return (
+    <>
+      {positioning.map((p) => {
+        return (
+          <div
+            key={p.event.id}
+            className={clsx(
+              "border-2 rounded-lg ${styles} text-xs  absolute overflow-hidden",
+              colorMap[p.event.color],
+            )}
+            style={{
+              top: `${p.top}px`,
+              height: `${p.height}px`,
+              left: `${p.left}%`,
+              width: `${p.width}%`,
+            }}
+          >
+            <EventBlock event={p.event} height={p.height} />
+          </div>
+        );
+      })}
+    </>
+  );
+}
