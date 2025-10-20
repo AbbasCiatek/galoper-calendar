85c9e1ceae555d9dc64356697b2b91dd529f1c9a
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-20 12:58:01 +0300

    deleted isALlDay const from event type, added needed functions in event store , renamed it ,added name for storage .

--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,9 @@
 export type ViewTypes = "day" | "week" | "month" | "year" | "agenda";
 export type COLORS =
-	| "blue"
-	| "green"
-	| "red"
-	| "yellow"
-	| "purple"
-	| "orange"
-	| "gray";
-
-export type Event = {
-	id: string;
-	title: string;
-	description: string;
-	startDate: Date;
-	endDate: Date;
-	color: COLORS;
-	isAllDay?: boolean;
-};
+  | "blue"
+  | "green"
+  | "red"
+  | "yellow"
+  | "purple"
+  | "orange"
+  | "gray";
