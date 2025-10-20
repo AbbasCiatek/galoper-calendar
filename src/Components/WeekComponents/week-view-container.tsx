ed69f202d5b0187293461db6b1dcb090ee1daf1c
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-20 11:19:28 +0300

    renamed event-store , deleted isAllDay boolean from event type , placed event type in event store

--- src/Components/WeekComponents/week-view-container.tsx
+++ src/Components/WeekComponents/week-view-container.tsx
@@ -3,12 +3,12 @@ import WeekTimeCells from "@/Components/WeekComponents/week-time-cells.tsx";
 import { ScrollArea } from "@/components/ui/scroll-area.tsx";
 
 export default function WeekViewContainer() {
-	return (
-		<ScrollArea className="h-[500px] md:h-[800px] ">
-			<div className="flex overflow-hidden">
-				<HoursColumn />
-				<WeekTimeCells />
-			</div>
-		</ScrollArea>
-	);
+  return (
+    <ScrollArea className="h-[500px] md:h-[800px] ">
+      <div className="flex overflow-hidden">
+        <HoursColumn />
+        <WeekTimeCells />
+      </div>
+    </ScrollArea>
+  );
 }
