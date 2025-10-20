dd21fcd2f3d902df12974298b284154cd9522959
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-20 10:41:39 +0300

    deleted default export

--- src/components/week-day-view-commons/date-display-layout.tsx
+++ src/components/week-day-view-commons/date-display-layout.tsx
@@ -1,8 +1,6 @@
 import type { ReactNode } from "react";
 
-export default function DateDisplayLayout({
-  children,
-}: { children: ReactNode }) {
+export function DateDisplayLayout({ children }: { children: ReactNode }) {
   return (
     <div className="relative z-20 flex border-b">
       <div className="w-18" />
