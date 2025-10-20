d9a32d035b001b1b5d3b145302adf72b9fc1587d
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-20 10:49:14 +0300

    solving conflict after rebase, kebab naming and deleting default exports

---
+++ src/components/week-components/week-time-cells.tsx
@@ -0,0 +1,24 @@
+import { useCalendar } from "@/context/calendar-context.tsx";
+import { daysOfWeek } from "@/dateHelpers.ts";
+import { isToday } from "date-fns";
+import { TimeCells } from "../week-day-view-commons/time-cell";
+import { TimeLine } from "../week-day-view-commons/time-line";
+
+export function WeekTimeCells() {
+  const { date } = useCalendar();
+  const weekDays = daysOfWeek(date);
+  return (
+    <div className="relative flex-1 border-l">
+      <div className="grid grid-cols-7 divide-x">
+        {weekDays.map((day) => {
+          return (
+            <div key={day.toISOString()} className="relative">
+              <TimeCells />
+              {isToday(day) && <TimeLine />}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
