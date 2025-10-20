d668db5bb82f09d4ad5ba349a7f910efd7ca432b
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-20 11:19:28 +0300

    renamed event-store , deleted isAllDay boolean from event type , placed event type in event store

--- src/Components/WeekComponents/event-per-day.tsx
+++ src/Components/WeekComponents/event-per-day.tsx
@@ -1,24 +1,26 @@
-import { endOfDay, startOfDay } from "date-fns";
 import EventsPositioning from "@/Components/WeeKDayViewCommonComponents/event-positioning.tsx";
-import useEventStore from "@/EventStore.ts";
+import { useEventStore } from "@/event-store.ts";
 import type { Event } from "@/types.ts";
+import { endOfDay, startOfDay } from "date-fns";
 
 export default function EventPerDay({ day }: { day: Date }) {
-	const { getSingleDayEvents } = useEventStore();
+  const { getEventsByDateRange } = useEventStore();
 
-	const events: Event[] = getSingleDayEvents(startOfDay(day), endOfDay(day));
+  const events: Array<Event> = getEventsByDateRange(
+    startOfDay(day),
+    endOfDay(day),
+  );
 
-	//the date is saved in store as Date and got as string .. need to cast it date object
-	const singleDayEvents = events.map((e) => ({
-		...e,
-		startDate: new Date(e.startDate),
-		endDate: new Date(e.endDate),
-	}));
-	return (
-		<>
-			{singleDayEvents.length > 0 && (
-				<EventsPositioning singleDayEvents={singleDayEvents} date={day} />
-			)}
-		</>
-	);
+  const singleDayEvents = events.map((e) => ({
+    ...e,
+    startDate: new Date(e.startDate),
+    endDate: new Date(e.endDate),
+  }));
+  return (
+    <>
+      {singleDayEvents.length > 0 && (
+        <EventsPositioning singleDayEvents={singleDayEvents} date={day} />
+      )}
+    </>
+  );
 }
