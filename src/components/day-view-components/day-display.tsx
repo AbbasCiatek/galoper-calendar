efaaf8be336a0afe1d9eff7f7227fcd8420adfe5
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-22 12:47:33 +0300

    addded Date_Format object, solved border issues

--- src/components/day-view-components/day-display.tsx
+++ src/components/day-view-components/day-display.tsx
@@ -1,18 +1,18 @@
 import { useCalendar } from "@/context/calendar-context.tsx";
+import { Date_Format } from "@/helpers";
 import { formatDate } from "date-fns";
 
 export function DayDisplay() {
   const { date } = useCalendar();
-  //"text-blue-500" : "text-muted-foreground"
   return (
     <div className="flex-1 border-l pt-2 pb-1 px-2">
       <div className="flex flex-col items-start text-xs font-medium  ">
         <div className="flex items-center gap-2">
           <span className="leading-none ">
-            {formatDate(date, "EE").toUpperCase()}
+            {formatDate(date, Date_Format.shortWeekDay).toUpperCase()}
           </span>
           <span className=" leading-none font-bold">
-            {formatDate(date, "d")}
+            {formatDate(date, Date_Format.dayOfMonth)}
           </span>
         </div>
       </div>
