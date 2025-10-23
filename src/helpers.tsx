d9805852b6efd57a5399a539a780ebea8d2ab091
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-23 09:23:26 +0300

    ensure uppercase fisrt char in capitalizeAndSlice

--- src/helpers.tsx
+++ src/helpers.tsx
@@ -15,5 +15,8 @@ export const DATE_FORMAT = {
 };
 export function capitalizeAndSlice(word: string, sliceAt: number) {
   if (!word) return "";
-  return (word.charAt(0) + word.slice(1).toLowerCase()).slice(0, sliceAt);
+  return (word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).slice(
+    0,
+    sliceAt,
+  );
 }
