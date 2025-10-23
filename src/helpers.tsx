c5fdbd804d37c45359f7eb064e08707f7b2e62bf
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-23 09:13:31 +0300

    clear function naming

--- src/helpers.tsx
+++ src/helpers.tsx
@@ -13,7 +13,7 @@ export const DATE_FORMAT = {
   fullDate: "dd MMM yyyy",
   dayOfMonth: "d",
 };
-export function lowerSliceWord(word: string, sliceAt: number) {
+export function capitalizeAndSlice(word: string, sliceAt: number) {
   if (!word) return "";
   return (word.charAt(0) + word.slice(1).toLowerCase()).slice(0, sliceAt);
 }
