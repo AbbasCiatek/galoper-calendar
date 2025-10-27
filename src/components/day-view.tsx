8ae8719ef73bfcbcee38616f48c0f30e7d680838
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-27 10:56:45 +0200

    removed unnecessary div, delete uppercase function and added uppercase utility class twcss

--- src/components/day-view.tsx
+++ src/components/day-view.tsx
@@ -5,11 +5,9 @@ import { DateDisplayLayout } from "./week-day-view-commons/date-display-layout";
 export function DayView() {
   return (
     <div className="flex flex-col border border-b-0 rounded-b-2xl">
-      <div>
-        <DateDisplayLayout>
-          <DayDisplay />
-        </DateDisplayLayout>
-      </div>
+      <DateDisplayLayout>
+        <DayDisplay />
+      </DateDisplayLayout>
       <DayViewContainer />
     </div>
   );
