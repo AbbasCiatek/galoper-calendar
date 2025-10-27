7ed6036cbf117df1ffd2400987dd5910f3c17ea8
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-27 09:10:04 +0200

    changed padding

--- src/components/day-view-components/day-display.tsx
+++ src/components/day-view-components/day-display.tsx
@@ -5,7 +5,7 @@ import { formatDate } from "date-fns";
 export function DayDisplay() {
   const { date } = useCalendar();
   return (
-    <div className="flex-1 border-l pt-2 pb-1 px-2">
+    <div className="flex-1 border-l p-1">
       <div className="flex flex-col items-start text-xs font-medium  ">
         <div className="flex items-center gap-2">
           <span className="leading-none ">
