c178a4b3edbafc7e55a16f6a53a5027cc70b18e8
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-27 08:50:23 +0200

    moved constants to constants file

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,7 @@
+export const HOURS = [
+  0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+  22, 23,
+];
 export const WEEK_DAYS = [
   "MONDAY",
   "TUESDAY",
@@ -12,4 +16,5 @@ export const DATE_FORMAT = {
   longMonth: "MMMM",
   fullDate: "dd MMM yyyy",
   dayOfMonth: "d",
+  hours24Format: "HH",
 };
