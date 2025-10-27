4b9bce91cb9bafb243d31b93bb335e7d01e75831
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-27 16:19:31 +0200

    file imported correctly

--- src/dnd/dnd-provider-wrapper.tsx
+++ src/dnd/dnd-provider-wrapper.tsx
@@ -1,4 +1,4 @@
-import CustomDragLayer from "@/dnd/custom-drag-layer.tsx";
+import { CustomDragLayer } from "@/dnd/custom-drag-layer.tsx";
 import type { ReactNode } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
