021e64db618c917c58579853d31075943c3ed037
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-27 16:11:40 +0200

    added dnd proveider wrapper, removed all default exports

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { CalendarHeader } from "@/components/calendar-header.tsx";
 import { DayView } from "@/components/day-view.tsx";
 import { CalendarContextProvider } from "@/context/calendar-context.tsx";
+import { DndProviderWrapper } from "./dnd/dnd-provider-wrapper";
 
 function App() {
   return (
+    <DndProviderWrapper>
     <CalendarContextProvider>
       <div className="container m-auto mt-5">
         <CalendarHeader />
         <DayView />
       </div>
     </CalendarContextProvider>
+    </DndProviderWrapper>
   );
 }
 
