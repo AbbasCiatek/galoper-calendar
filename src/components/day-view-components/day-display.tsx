039adde3409d0e0f9e96a26b29cb7964bdc066d8
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-27 10:56:45 +0200

    removed unnecessary div, delete uppercase function and added uppercase utility class twcss

--- src/components/day-view-components/day-display.tsx
+++ src/components/day-view-components/day-display.tsx
@@ -8,8 +8,8 @@ export function DayDisplay() {
     <div className="flex-1 border-l p-1">
       <div className="flex flex-col items-start text-xs font-medium  ">
         <div className="flex items-center gap-2">
-          <span className="leading-none ">
-            {formatDate(date, DATE_FORMAT.shortWeekDay).toUpperCase()}
+          <span className="leading-none uppercase ">
+            {formatDate(date, DATE_FORMAT.shortWeekDay)}
           </span>
           <span className=" leading-none font-bold">
             {formatDate(date, DATE_FORMAT.dayOfMonth)}
