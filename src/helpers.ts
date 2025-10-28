f95db536713148ed8e900e37f3b03b635dd28082
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-28 09:10:36 +0200

    deleted unUsed function

--- src/helpers.ts
+++
@@ -1,7 +0,0 @@
-export function capitalizeAndSlice(word: string, sliceAt: number) {
-  if (!word) return "";
-  return (word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).slice(
-    0,
-    sliceAt,
-  );
-}
