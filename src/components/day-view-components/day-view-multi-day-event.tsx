96e7c63f53ab66b08341434f37002249713474d5
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-29 14:43:20 +0200

    replaced let with const and condition in event-positioning.tsx, replaced interface with type,condition of undisplayed events fixed, and separate logic from map function into another component

--- src/components/day-view-components/day-view-multi-day-event.tsx
+++ src/components/day-view-components/day-view-multi-day-event.tsx
@@ -40,6 +40,9 @@ export function DayViewMultiDayEvent() {
     return allAndMultiDayEventsInDay;
   }, [multiDayEvents, allDayEvents, showMore]);
 
+  const undisplayedEvents =
+    multiDayEvents.length + allDayEvents.length - MAX_ALL_AND_MULTI_DAY_EVENTS;
+
   return (
     <div className="flex border-b">
       <AnimatePresence initial={false}>
@@ -49,22 +52,18 @@ export function DayViewMultiDayEvent() {
           transition={{ duration: 0.4, ease: "easeInOut" }}
           className="flex w-18 justify-center items-end "
         >
-          {multiDayEvents.length +
-            allDayEvents.length -
-            MAX_ALL_AND_MULTI_DAY_EVENTS >
-            0 &&
-            !showMore && (
-              <motion.button
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                exit={{ opacity: 0, scale: 0.5 }}
-                className="flex items-center justify-center size-10 rounded-full hover:bg-gray-200"
-                type="button"
-                onClick={() => setShowMore(!showMore)}
-              >
-                <ChevronDown className="text-gray-700" />
-              </motion.button>
-            )}
+          {undisplayedEvents > 0 && !showMore && (
+            <motion.button
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.5 }}
+              className="flex items-center justify-center size-10 rounded-full hover:bg-gray-200"
+              type="button"
+              onClick={() => setShowMore(!showMore)}
+            >
+              <ChevronDown className="text-gray-700" />
+            </motion.button>
+          )}
           {multiDayEvents.length +
             allDayEvents.length -
             MAX_ALL_AND_MULTI_DAY_EVENTS >
@@ -85,83 +84,79 @@ export function DayViewMultiDayEvent() {
         <ScrollArea className=" flex-1 border-l w-full max-h-32 ">
           <div className="flex flex-col gap-1 py-1">
             {memoizedEvents.map((event, index) => {
-              if (!isSameDay(event.startDate, event.endDate)) {
-                const eventStart = startOfDay(new Date(event.startDate));
-                const eventEnd = startOfDay(new Date(event.endDate));
-                const currentDate = startOfDay(date);
-
-                const eventTotalDays =
-                  differenceInDays(eventEnd, eventStart) + 1;
-                const eventCurrentDay =
-                  differenceInDays(currentDate, eventStart) + 1;
-
-                return (
-                  <motion.div
-                    initial={{ opacity: 0, x: -10 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    transition={{
-                      duration: 0.2,
-                      delay: 0.1 * index,
-                      type: "spring",
-                      stiffness: 200,
-                      damping: 20,
-                      mass: 2,
-                    }}
-                    key={event.id}
-                  >
-                    <MultiDayEventDisplay
-                      event={event}
-                      eventCurrentDay={eventCurrentDay}
-                      eventTotalDays={eventTotalDays}
-                    />
-                  </motion.div>
-                );
-              }
-
-              return (
-                <motion.div
-                  initial={{ opacity: 0, x: -10 }}
-                  animate={{ opacity: 1, x: 0 }}
-                  transition={{
-                    duration: 0.2,
-                    delay: 0.1 * index,
-                    type: "spring",
-                    stiffness: 200,
-                    damping: 20,
-                    mass: 2,
-                  }}
-                  key={event.id}
-                >
-                  <MultiDayEventDisplay event={event} />
-                </motion.div>
-              );
+              return <NamingEvent event={event} index={index} key={event.id} />;
             })}
-            {!showMore &&
-              multiDayEvents.length +
-                allDayEvents.length -
-                MAX_ALL_AND_MULTI_DAY_EVENTS >
-                0 && (
-                <motion.button
-                  type="button"
-                  onClick={() => setShowMore(true)}
-                  initial={{ opacity: 0 }}
-                  animate={{ opacity: 1 }}
-                  transition={{ delay: 0.15 }}
-                  className="flex justify-center rounded-full  text-gray-600 cursor-pointer text-xs font-semibold "
-                >
-                  <span>
-                    {" "}
-                    +
-                    {multiDayEvents.length +
-                      allDayEvents.length -
-                      MAX_ALL_AND_MULTI_DAY_EVENTS}{" "}
-                    <span className="hidden lg:inline-block">more</span>
-                  </span>
-                </motion.button>
-              )}
+            {!showMore && undisplayedEvents > 0 && (
+              <motion.button
+                type="button"
+                onClick={() => setShowMore(true)}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.15 }}
+                className="flex justify-center rounded-full  text-gray-600 cursor-pointer text-xs font-semibold "
+              >
+                <span>
+                  {" "}
+                  +{undisplayedEvents}{" "}
+                  <span className="hidden lg:inline-block">more</span>
+                </span>
+              </motion.button>
+            )}
           </div>
         </ScrollArea>
       </AnimatePresence>
     </div>
+  );
+}
+
+function NamingEvent({ event, index }: { event: Event; index: number }) {
+  if (!isSameDay(event.startDate, event.endDate)) {
+    const { date } = useCalendar();
+
+    const eventStart = startOfDay(new Date(event.startDate));
+    const eventEnd = startOfDay(new Date(event.endDate));
+    const currentDate = startOfDay(date);
+
+    const eventTotalDays = differenceInDays(eventEnd, eventStart) + 1;
+    const eventCurrentDay = differenceInDays(currentDate, eventStart) + 1;
+
+    return (
+      <motion.div
+        initial={{ opacity: 0, x: -10 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{
+          duration: 0.2,
+          delay: 0.1 * index,
+          type: "spring",
+          stiffness: 200,
+          damping: 20,
+          mass: 2,
+        }}
+        key={event.id}
+      >
+        <MultiDayEventDisplay
+          event={event}
+          eventCurrentDay={eventCurrentDay}
+          eventTotalDays={eventTotalDays}
+        />
+      </motion.div>
+    );
+  }
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, x: -10 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={{
+        duration: 0.2,
+        delay: 0.1 * index,
+        type: "spring",
+        stiffness: 200,
+        damping: 20,
+        mass: 2,
+      }}
+    >
+      <MultiDayEventDisplay event={event} />
+    </motion.div>
   );
 }
