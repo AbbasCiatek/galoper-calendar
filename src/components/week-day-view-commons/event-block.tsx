8e3598ab5a98bdc96a12058a7a88a731336c68e4
HusseinMShamas <https://github.com/HusseinMShamas>
2025-10-30 09:42:17 +0200

    endDate displayed correctly

--- src/components/week-day-view-commons/event-block.tsx
+++ src/components/week-day-view-commons/event-block.tsx
@@ -9,7 +9,7 @@ export function EventBlock({ event }: { event: Event }) {
       <div className=" overflow-hidden text-ellipsis">
         <div className="truncate font-semibold">{event.title}</div>
         <div className="px-1">
-          {`${formatDate(event.startDate, DATE_FORMAT.timeFormat)} - $formatDate(event.endDate, DATE_FORMAT.timeFormat)`}
+          {`${formatDate(event.startDate, DATE_FORMAT.timeFormat)} - ${formatDate(event.endDate, DATE_FORMAT.timeFormat)}`}
         </div>
       </div>
     </DraggableEvents>
